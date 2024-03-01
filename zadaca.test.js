669fffbf66686e25fcc8f66b23f3908ac58c470a
Ivan Ivanovski <ivan.ivanovski@students.finki.ukim.mk>
2024-03-02 00:03:01 +0100

    Old content is removed. Now the script is show alert if user enter name and age

--- zadaca.test.js
+++ zadaca.test.js
@@ -1,2 +1,14 @@
-let org = "Codders";
-console.log("Hello to "+org);
+//input for name 
+let name = prompt("Enter your name");
+//input for age
+let age = prompt("Enter your age");
+//validation for name, age
+if(name == '' || parseInt(age) || age=='') {
+   //display alert error if name or age are not entered from client or age is not integer
+   alert("You need to enter name and age");
+}
+else {
+   //display success alert to client
+   alert(`Hello ${name} ! Nice to see you`);
+}
+
